3547093bcb6ec8f74e0b95d4851a06e0eb0c3713
Godwill993 <lendzelegoodwill@gmail.com>
2025-09-17 20:13:47 +0100

    dshh

--- mode.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggle-mode').onclick = function() {
-    const style = document.getElementById('theme-style');
-  if(style.innerHTML.includes('#fff')) {
-    style.innerHTML = 'body { background: #222; color: #fff; }';
-  } else {
-    style.innerHTML = 'body { background: #fff; color: #000; }';
-  }
-};
